f63f85d1453bae380f7f5457b29e0d1699384f41
OstapoKapo <OstapoKapo@gmail.com>
2023-08-29 00:14:39 +0300

    change passwords

--- src/components/MainPage/SeePassword/PasswordItems/PasswordItems.jsx
+++ src/components/MainPage/SeePassword/PasswordItems/PasswordItems.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import './PasswordItems.css';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import CryptoJS from 'crypto-js'
 import copyImg from './img/copy.png';
 import editImg from './img/edit.png';
 import deleteImg from './img/delete.png';
 
 export default function PasswordItems(props) {
+
+  const navigate = useNavigate();
+
+  let email = props.email;
+
 
   const secretPass = "XkhZG4fW2t2W";
 
@@ -26,10 +33,27 @@ export default function PasswordItems(props) {
         console.log('ddd')
       }, 3000);
    }
+
+   const  deleteHandle = async (e) => {
+      let target = e.target;
+      let mainParent = target.closest('.contentBlock');
+      let mainParentId = mainParent.id;
+      await axios.post('http://localhost:3001/deleteApp', {mainParentId, email})
+      .then((response) => {
+        if(response.status !== 404){ 
+          props.setPasswords(response.data)
+        }else{
+          alert('somethin went wrong');
+        }
+      })
+      .catch((error) => {
+          console.error('Error:', error);
+      }); 
+   }
   
 
   return (
-    <div className="contentBlock">
+    <div className="contentBlock" id={props.appName}>
       <div className="contentBlock__text">{props.appName}</div>
       <input type="password" className='contentBlock__input' value={descryptPassword}/>
       <div className="contentBlock__row">
@@ -39,7 +63,7 @@ export default function PasswordItems(props) {
           <div className="contentBlock__img">
             <img src={editImg} alt="copy" />
           </div>
-          <div className="contentBlock__img">
+          <div className="contentBlock__img" onClick={deleteHandle}>
             <img src={deleteImg} alt="copy" />
           </div>
         </div>
